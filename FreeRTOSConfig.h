92d265f48dcd44b3ca9f7cad115587667016691f
Matt Young <matt.young.1@outlook.com>
2022-02-03 01:04:17 +1000

    Updated FreeRTOS to 10.4.6 (untested)
    Please test this compiles and works with the NXP debug soon!

--- FreeRTOSConfig.h
+++ FreeRTOSConfig.h
@@ -52,7 +52,7 @@
 #define configUSE_TASK_NOTIFICATIONS				1
 #define configUSE_MUTEXES							1
 #define configUSE_RECURSIVE_MUTEXES					0
-#define configUSE_COUNTING_SEMAPHORES				1
+#define configUSE_COUNTING_SEMAPHORES				0
 #define configUSE_ALTERNATIVE_API					0 /* Deprecated! */
 #define configQUEUE_REGISTRY_SIZE					8
 #define configUSE_QUEUE_SETS						0
